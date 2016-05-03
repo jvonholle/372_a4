bd02f08255e681ee30710ca106dfb0e23a1aa967
James Von Holle <jrvonhollejr@alaska.edu>
2016-05-03 17:45:12 +0000

    adding a couple more maps and new death message

--- src/action.cpp
+++ src/action.cpp
@@ -53,15 +53,15 @@ void Action::turn(const bool & can_do){
     if(can_do){
         current_->print();
         if(current_->is_bad()){
-            std::cout << "▄██   ▄    ▄██████▄  ███    █▄       ████████▄   ▄█     ▄████████ ████████▄"   << std::endl
-                      << "███   ██▄ ███    ███ ███    ███      ███   ▀███ ███    ███    ███ ███   ▀███"  << std::endl
-                      << "███▄▄▄███ ███    ███ ███    ███      ███    ███ ███▌   ███    █▀  ███    ███"  << std::endl
-                      << "▀▀▀▀▀▀███ ███    ███ ███    ███      ███    ███ ███▌  ▄███▄▄▄     ███    ███"  << std::endl
-                      << "▄██   ███ ███    ███ ███    ███      ███    ███ ███▌ ▀▀███▀▀▀     ███    ███"  << std::endl
-                      << "███   ███ ███    ███ ███    ███      ███    ███ ███    ███    █▄  ███    ███"  << std::endl
-                      << "███   ███ ███    ███ ███    ███      ███   ▄███ ███    ███    ███ ███   ▄███"  << std::endl
-                      << " ▀█████▀   ▀██████▀  ████████▀       ████████▀  █▀     ██████████ ████████▀ "  << std::endl; // ascii art from http://www.patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
-                                                                                                                      //  Looked into FIGlet, but didn't have time to compile for my system.
+            std::cout <<
+                           "oooooo   oooo   .oooooo.   ooooo     ooo      oooooooooo.   ooooo oooooooooooo oooooooooo."    << std::endl
+                        << " `888.   .8'   d8P'  `Y8b  `888'     `8'      `888'   `Y8b  `888' `888'     `8 `888'   `Y8b"   << std::endl
+                        << "  `888. .8'   888      888  888       8        888      888  888   888          888      888"  << std::endl
+                        << "   `888.8'    888      888  888       8        888      888  888   888oooo8     888      888"  << std::endl
+                        << "    `888'     888      888  888       8        888      888  888   888    \"     888      888" << std::endl
+                        << "     888      `88b    d88'  `88.    .8'        888     d88'  888   888       o  888     d88'"  << std::endl
+                        << "    o888o      `Y8bood8P'     `YbodP'         o888bood8P'   o888o o888ooooood8 o888bood8P'"    << std::endl;// ascii art from http://www.patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+                                                                                                                      //  Looked into FIGlet, but didn't have time to compile for my system. This website does use FIGlet fonts though
                                                                              
 
             return;
